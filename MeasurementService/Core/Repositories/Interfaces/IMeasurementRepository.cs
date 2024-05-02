90c6f3806345804c91d5756521465094f3867771
Nick <nicklas.nick@live.dk>
2024-05-02 03:53:11 +0200

    GetMeasurementBySsn Works

--- MeasurementService/Core/Repositories/Interfaces/IMeasurementRepository.cs
+++ MeasurementService/Core/Repositories/Interfaces/IMeasurementRepository.cs
@@ -4,7 +4,7 @@ namespace MeasurementService.Core.Repositories.Interfaces;
 
 public interface IMeasurementRepository
 {
-    public Task<IEnumerable<DBEntities.Measurement>> GetAllMeasurementsByPatientId(int id);
+    public Task<IEnumerable<DBEntities.Measurement>> GetAllMeasurementsBySsn(string ssn);
     public Task<DBEntities.Measurement> GetMeasurementById(int measurementId);
     public Task AddMeasurements(DBEntities.Measurement measurement);
     public Task DeleteMeasurement(int measurementId);
--- MeasurementService/Interfaces/IMeasurementService.cs
+++ MeasurementService/Interfaces/IMeasurementService.cs
@@ -5,7 +5,7 @@ namespace MeasurementService.Interfaces;
 
 public interface IMeasurementService
 {
-    Task<IEnumerable<DBEntities.Measurement>> GetAllMeasurementsByPatientId(int patientId); // For fetching all measurements
+    Task<IEnumerable<DBEntities.Measurement>> GetAllMeasurementsBySsn(string ssn); // For fetching all measurements
     Task<DBEntities.Measurement> GetMeasurementById(int measurementId); // For fetching a specific measurement
     Task AddMeasurements(MeasurementDTO measurementDTO); // For adding a new measurement
     Task DeleteMeasurement(int measurementId); // For deleting a specific measurement
