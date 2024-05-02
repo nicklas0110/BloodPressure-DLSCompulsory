39d9b112d847780017b2ef5bbab290a4488bc3e3
mols14 <71692370+mols14@users.noreply.github.com>
2024-05-02 15:58:41 +0200

    Featurehub Service and setup

---
+++ FeatureHub/FeatureHubClient.cs
@@ -0,0 +1,18 @@
+﻿using FeatureHubSDK;
+
+namespace FeatureHub;
+
+public class FeatureHubClient
+{
+    private readonly IFeatureHubConfig _config;
+
+    public FeatureHubClient(string apikey)
+    {
+        FeatureLogging.DebugLogger += (sender, s) => Console.WriteLine("Debug: " + s);
+        FeatureLogging.TraceLogger += (sender, s) => Console.WriteLine("Trace: " + s);
+        FeatureLogging.InfoLogger   += (sender, s) => Console.WriteLine("Info: " + s);
+        FeatureLogging.ErrorLogger  += (sender, s) => Console.WriteLine("Error: " + s);
+
+        _config = new EdgeFeatureHubConfig("http://featurehub:8085", apikey);
+    }
+}
---
+++ FeatureHub/FeatureHubFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace FeatureHub;
+
+public class FeatureHubFactory
+{
+    public static FeatureHubClient CreateFeatureHub()
+    {
+        const string apikey = "c4d231ae-e411-44f9-863b-9ec880abed4e/Ir0mH6xQzjSRdlAj85tLRztNwywcfrETo4PdV7RW";
+        return new FeatureHubClient(apikey);
+    }
+}
